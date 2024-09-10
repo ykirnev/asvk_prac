1065bfb3ba2aa46e8e3d2b337b9c8a82f247fd2c
Юрий Кирнев <ykirnev@gmail.com>
2024-09-10 11:52:41 +0300

    python_start + tests

---
+++ 20240910/0/1.py
@@ -0,0 +1,10 @@
+x = input()
+print (x, x + x, type(x))
+a = 1
+b = 3
+print(eval ("a*2 + b"))
+s = input()
+print(eval(s))
+print(dir())
+t = eval(input())
+print(type(t), t)
---
+++ 20240910/0/2.py
@@ -0,0 +1,3 @@
+a, b, c = eval(input())
+print(type(a), type(b), type(c))
+#1, "121", print
