93a99c5c099030350c592c39c82e598c33b541ad
Юрий Кирнев <ykirnev@gmail.com>
2024-10-03 12:10:35 +0300

    Homework4 upd

--- 20241001/2/prog.py
+++ 20241001/2/prog.py
@@ -7,3 +7,24 @@ def difference(a, b):
             return tuple(res)
         else:
             return res
+
+x = input()
+left_pos = x.find('(') if '(' in x else x.find('[')
+if left_pos == -1:
+    a, b = x.split(',', 1)
+else:
+    depth = 0
+    for i, char in enumerate(x):
+        if char in '([':
+            depth += 1
+        elif char in ')]':
+            depth -= 1
+        elif char == ',' and depth == 0:
+            a = x[:i]
+            b = x[i+1:].strip()
+            break
+a = eval(a)
+b = eval(b)
+res = difference(a, b)
+print(res)
+
--- 20241001/3/prog.py
+++ 20241001/3/prog.py
@@ -3,7 +3,7 @@ def Calc(s, t, u):
     def func(x):
         return eval(u, {"x": eval(s), "y": eval(t)})
     return func
-s, t, u = input().split(',')
+s, t, u = input().replace('"', "").split(',')
 x = eval(input())
 F = Calc(s, t, u)
 result = F(x)
