1b74b5ab2def53432b378a85ef070c0b28aad465
Юрий Кирнев <ykirnev@gmail.com>
2025-03-16 19:11:11 +0300

    Cmd addition

--- 20250310/1/prog.py
+++ 20250310/1/prog.py
@@ -1,6 +1,7 @@
 import cowsay
 from io import StringIO
 import shlex
+import cmd
 from collections import namedtuple
 
 jgsbat = cowsay.read_dot_cow(StringIO( r"""
@@ -16,12 +17,16 @@ jgsbat = cowsay.read_dot_cow(StringIO( r"""
 
 """))
 
-class MUDGame:
+class MUDGame(cmd.Cmd):
+    prompt = "(MUD) "
+
     def __init__(self):
+        super().__init__()
         self.player_pos = (0, 0)
         self.monsters = {}
 
-    def move(self, direction):
+    def do_move(self, direction):
+        """Move the player in a direction: up, down, left, right."""
         x, y = self.player_pos
         if direction == "up":
             y = (y - 1) % 10
@@ -32,30 +37,27 @@ class MUDGame:
         elif direction == "right":
             x = (x + 1) % 10
         else:
-            print("Invalid command")
+            print("Invalid direction")
             return
 
         self.player_pos = (x, y)
         print(f"Moved to ({x}, {y})")
         self.encounter(x, y)
 
-    def add_monster(self, command):
-        tokens = shlex.split(command)
-
-        if tokens[0] != "addmon":
-            print("Invalid command")
+    def do_addmon(self, args):
+        """Add a monster with a name, hp, coordinates, and a greeting."""
+        tokens = shlex.split(args)
+        if not tokens:
+            print("Usage: addmon <name> hp <value> coords <x> <y> hello <message>")
             return
 
         monster_data = {}
-        i = 1
+        i = 0
         while i < len(tokens):
-            if tokens[i] == "hello":
-                monster_data["hello"] = tokens[i + 1]
-                i += 2
-            elif tokens[i] == "hp":
+            if tokens[i] == "hp":
                 try:
                     monster_data["hp"] = int(tokens[i + 1])
-                except ValueError:
+                except (ValueError, IndexError):
                     print("Invalid value for hp")
                     return
                 i += 2
@@ -63,26 +65,33 @@ class MUDGame:
                 try:
                     monster_data["coords"] = (int(tokens[i + 1]), int(tokens[i + 2]))
                 except (ValueError, IndexError):
-                    print("Invalid value for coords")
+                    print("Invalid coordinates")
                     return
                 i += 3
+            elif tokens[i] == "hello":
+                monster_data["hello"] = tokens[i + 1]
+                i += 2
             else:
                 monster_data["name"] = tokens[i]
                 i += 1
 
+        if "name" not in monster_data or "hp" not in monster_data or "coords" not in monster_data:
+            print("Invalid monster definition")
+            return
 
         name = monster_data["name"]
-        hello = monster_data["hello"]
+        hello = monster_data.get("hello", "Hello!")
         hp = monster_data["hp"]
         x, y = monster_data["coords"]
 
-        self.monsters[(x, y)] = (name, hello, hp)
-        print(f"Added monster {name} to ({x}, {y}) saying {hello} with {hp} HP")
+        self.monsters[(x, y)] = {"name": name, "hello": hello, "hp": hp}
+        print(f"Added monster {name} to ({x}, {y}) saying '{hello}' with {hp} HP")
 
     def encounter(self, x, y):
         if (x, y) in self.monsters:
-            name, hello, hp = self.monsters[(x, y)]
-            print(cowsay.cow(hello))
+            monster = self.monsters[(x, y)]
+            print(cowsay.cow(monster["hello"]))
+
 
 def addmon(game, monster_name, x, y, hello):
     if monster_name == "jgsbat":
@@ -90,7 +99,5 @@ def addmon(game, monster_name, x, y, hello):
     else:
         game.add_monster(monster_name, x, y, hello)
 
-
-print("<<< Welcome to Python-MUD 0.1 >>>")
-game = MUDGame()
-game.add_monster('addmon dragon hp 999 coords 6 9 hello "Who goes there?"')
+print("<<< Welcome to Python-MUD 0.2 >>>")
+MUDGame().cmdloop()
